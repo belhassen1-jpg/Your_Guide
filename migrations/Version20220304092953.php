ece88062fff95fdb45f4f925eb876ea9612c64e0
unknown <hamza.benayed@esprit.tn>
2022-03-04 10:40:41 +0100

    second commit

---
+++ migrations/Version20220304092953.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220304092953 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle LONGTEXT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reclamation (id INT AUTO_INCREMENT NOT NULL, categorie_id INT NOT NULL, date_rec DATE NOT NULL, type LONGTEXT NOT NULL, description LONGTEXT NOT NULL, etat LONGTEXT NOT NULL, INDEX IDX_CE606404BCF5E72D (categorie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reclamation ADD CONSTRAINT FK_CE606404BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reclamation DROP FOREIGN KEY FK_CE606404BCF5E72D');
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE reclamation');
+    }
+}
